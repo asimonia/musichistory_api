43518359ce8b6aeab52892e74450a4f72bc56600
asimonia <alex.simonian@gmail.com>
2017-02-03 12:33:41 -0600

    Added ViewSets

--- musichistory/musichistory_api/views.py
+++ musichistory/musichistory_api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from musichistory_api import serializers, models
+from rest_framework import viewsets
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+	queryset = User.objects.all()
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+	queryset = models.Genre.objects.all()
+	serializer_class = serializers.GenreSerializer
+
+
+class ArtistViewSet(viewsets.ModelViewSet):
+	queryset = models.Artist.objects.all().order_by('-name')
+	serializer_class = serializers.ArtistSerializer
+
+
+class AlbumViewSet(viewsets.ModelViewSet):
+	queryset = models.Album.objects.all().order_by('-artist')
+	serializer_class = serializers.AlbumSerializer
+
+
+class SongViewSet(viewsets.ModelViewSet):
+	queryset = models.Song.objects.all().order_by('-album')
+	serializer_class = serializers.SongSerializer
