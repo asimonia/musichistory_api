c87d4a5e702c774e528d022208bb53e20fa150d1
asimonia <alex.simonian@gmail.com>
2017-02-03 12:17:02 -0600

    Changes to Genre model

--- musichistory/musichistory_api/models.py
+++ musichistory/musichistory_api/models.py
@@ -7,26 +7,7 @@ class Genre(models.Model):
 	The genres table contains the various genres related to the songs.  Songs
 	can fit into more than one genre, or none at all.
 	"""
-	NONE = ''
-	POP = 'Pop'
-	ROCK = 'Rock'
-	METAL = 'Metal'
-	JAZZ = 'Jazz'
-	COUNTRY = 'Country'
-	GOSPEL = 'Gospel'
-
-	GENRE_TYPE_CHOICES = (
-		(NONE, ''),
-		(POP, 'Pop'),
-		(ROCK, 'Rock'),
-		(METAL, 'Metal'),
-		(JAZZ, 'Jazz'),
-		(COUNTRY, 'Country'),
-		(GOSPEL, 'Gospel'),
-	)
-
-	label = models.CharField(max_length=20, choices=GENRE_TYPE_CHOICES, default=NONE)
-	description = models.TextField(max_length=4000)
+	label = models.CharField(max_length=20, unique=True)
 
 	class Meta:
 		verbose_name_plural = 'Genres'
@@ -54,11 +35,12 @@ class Album(models.Model):
 	This table contains the albums and the related information.
 	"""
 	title = models.CharField(max_length=40)
-	release_date = models.DateField()
-	album_length = models.TimeField()
+	release_date = models.DateField('release date')
+	album_length = models.DurationField('album length')
 	num_stars = models.IntegerField()
 	label = models.CharField(max_length=40)
 	artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+	genres = models.ManyToManyField(Genre)
 
 	class Meta:
 		verbose_name_plural = 'Albums'
@@ -72,12 +54,12 @@ class Song(models.Model):
 	This table contains a list of songs and the related information.
 	"""
 	title = models.CharField(max_length=40)
-	song_length = models.TimeField()
-	release_date = models.DateField()
+	song_length = models.DurationField('track length')
+	release_date = models.DateField('release date')
+	plays = models.IntegerField(default=0)
 	artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
 	album = models.ForeignKey(Album, on_delete=models.CASCADE)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	genres = models.ManyToManyField(Genre)
 
 	class Meta:
 		verbose_name_plural = 'Songs'
