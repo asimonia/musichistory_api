dfe9682074b8a3210559345714ea8af51455e18d
asimonia <alex.simonian@gmail.com>
2017-02-03 10:57:57 -0600

    Added admin panel

--- musichistory/musichistory_api/admin.py
+++ musichistory/musichistory_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from musichistory_api.models import Album, Artist, Genre, Song
 
-# Register your models here.
+
+admin.site.register(Album)
+admin.site.register(Artist)
+admin.site.register(Genre)
+admin.site.register(Song)
